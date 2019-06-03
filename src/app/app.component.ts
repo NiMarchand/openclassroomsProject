e2ff1687d6a57388f246a9c03daaf1cb9b02e069
nmarchand <nmarchand@micropole.com>
2019-06-03 16:06:04 +0200

    firstCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'post';
+  appName = 'Post-it';
+
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: 'Information : je vous conseille d\'utiliser Bootstrap pour cet exercice',
+      loveIts: 1,
+      created_at: new Date('01/05/1998 21:54')
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Information : je vous conseille d\'utiliser Bootstrap pour cet exercice',
+      loveIts: 0,
+      created_at: new Date('01/05/2006 08:02')
+    },
+    {
+      title: 'Mon troisième post',
+      content: 'Information : je vous conseille d\'utiliser Bootstrap pour cet exercice',
+      loveIts: -1,
+      created_at: new Date('12/08/2009 12:05')
+    }
+  ];
 }
